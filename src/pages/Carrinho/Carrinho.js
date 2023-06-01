543a22c7426fee2130239b7c820de84a41803253
mavis-martins <mavismr98@gmail.com>
2023-06-01 08:24:27 -0300

    atualização Carrinho

--- src/pages/Carrinho/Carrinho.js
+++ src/pages/Carrinho/Carrinho.js
@@ -2,6 +2,10 @@ function Carrinho () {
     return (
         <div>
             <h1>Carrinho:</h1>
+            <p>
+                Aqui ficará os itens da sua compra. Você poderá adicionar ou remover quantidades dos itens, revisar os itens, cancelar,
+                ou finalizar sua compra.
+            </p>
         </div>
     )
 }
